ceee1f688a81d5d2d26f7636a740c98db104bc12
Alok Dhole <alokdhole2@gmail.com>
2026-07-08 13:59:08 +0530

    Dynamic sitemap added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { products } from "@/data/products";
+import { products } from "./data/products";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.prakumbhclothing.com";
